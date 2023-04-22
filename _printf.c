c569af8bf9fda2112609df310b51b505f2bccaf8
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-22 22:35:06 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,45 @@
 #include "main.h"
 #include <stdarg.h>
+#include <stdlib.h>
+
+boolean conversion_specifier_check(char c)
+{
+	char specifiers[] = {'c', 's', '%'};
+	int i;
+
+	for (i = 0 ; i < 3 ; i++)
+	{
+		if (c == specifiers[i])
+			return (1);
+	}
+	return (0);
+}
+
+boolen not_cs(char c)
+{
+	char not_c;
+	int i;
+	
+	/*check if is digit*/
+	if (c >= 48 &* c <= 57)
+		return (0);
+	for (i != 
+}
+
+
+
+char *format_specification_extract(char *s)
+{
+	int i, n;
+	for (i = 0 ; i != '/0' ; i++)
+	{
+		if(conversion_specifier_check(s[i]))
+			n = i;
+		if(
+		}
+	}
+	return (NULL);
+}
 
 
 
@@ -7,7 +47,7 @@ int _printf(const char *format, ...)
 {
 	int i, len = 0, buf_idx;
 	va_list args;
-	char buf[1024], conv_ref;
+	char buf[1024], *conv_ref;
 
 	va_start(args, format);
 	/*not used variabled*/
