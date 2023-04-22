fbec0abc8c217c574e0bee9de94fbf57dd0add67
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-22 07:55:55 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,11 +1,14 @@
 #include "main.h"
 int _printf(const char *format, ...)
 {
-	int i, len = 0;
+	int i, len = 0, buf_idx;
+	va_list args;
 	char buf[1024];
 
+	va_start(args, format);
 	/*not used variabled*/
 	(void) buf;
+	(void) buf_idx;
 
 	for (i = 0; format[i] != '\0' ; i++)
 	{
