d9bffc8dc74e99fbe2aec1d3d7902c044c1b8790
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-24 03:34:04 +0200

    Update conversions.c

--- conversions.c
+++ conversions.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ * uint_to_string - convert int to string
+ * @num: the integer number
+ * @buf: number buffer
+ *
+ * Return: length of the string
+ */
+int uint_to_string(unsigned int num, char *buf)
+{
+	int i, j = 0;
+	int digits_array[32];
+
+	if (num == 0)
+	{
+		buf[0] = '0';
+		buf[1] = '\0';
+		return (1);
+	}
+
+	for (i = 0 ; num > 0 ; i++)
+	{
+		digits_array[i] = num % 10;
+		num = num / 10;
+	}
+
+	for (i = i - 1; i >= 0 ; i--)
+	{
+		buf[j] = digits_array[i] + '0';
+		j++;
+	}
+
+	buf[j] = '\0';
+
+	return (j);
+}
+
+/**
  * int_to_octal - convert int to string
  * @num: the integer number
  * @buf: number buffer
