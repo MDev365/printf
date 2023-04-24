adbbc37455651ee73301656e55adfcd9adc37911
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-24 03:34:19 +0200

    Update main.h

--- main.h
+++ main.h
@@ -5,6 +5,7 @@ int _strlen(const char *s);
 int int_to_string(int num, char *buf);
 int int_to_binary(unsigned int num, char *buf);
 int int_to_octal(unsigned int num, char *buf);
+int uint_to_string(unsigned int num, char *buf);
 int print_string(char *s);
 int _printf(const char *format, ...);
 #endif
