e2374458e23da6ae97dd73a3c5fba84ba0a07861
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-24 03:30:02 +0200

    Update conversions.c

--- conversions.c
+++ conversions.c
@@ -1,6 +1,42 @@
 #include "main.h"
 
 /**
+ * int_to_octal - convert int to string
+ * @num: the integer number
+ * @buf: number buffer
+ *
+ * Return: length of the string
+ */
+int int_to_octal(unsigned int num, char *buf)
+{
+	int i, j = 0;
+	int digits_array[32];
+
+	if (num == 0)
+	{
+		buf[0] = '0';
+		buf[1] = '\0';
+		return (1);
+	}
+
+	for (i = 0 ; num > 0 ; i++)
+	{
+		digits_array[i] = num % 8;
+		num = num / 8;
+	}
+
+	for (i = i - 1; i >= 0 ; i--)
+	{
+		buf[j] = digits_array[i] + '0';
+		j++;
+	}
+
+	buf[j] = '\0';
+
+	return (j);
+}
+
+/**
  * int_to_binary - convert int to string
  * @num: the integer number
  * @buf: number buffer
