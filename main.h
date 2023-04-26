1dfa4c7dc8432389688fddb495e167e6dfd2d0d5
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-26 02:27:12 +0200

    Update main.h

--- main.h
+++ main.h
@@ -1,12 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
+/* std libs */
+#include <stdarg.h>
+#include <stdlib.h>
+
+/* putchar and print functions */
 int _putchar(char c);
+int print_address(void *p);
+int print_string_non_print(char *s);
+int print_reverse_string(char *s);
+int print_rot13ed_string(char *s);
+int print_string(char *s);  
+
+/* conversions functions */
 int _strlen(const char *s);
 int int_to_string(int num, char *buf);
 int int_to_binary(unsigned int num, char *buf);
 int int_to_octal(unsigned int num, char *buf);
 int int_to_hex(unsigned int num, char *buf, int cap);
 int uint_to_string(unsigned int num, char *buf);
-int print_string(char *s);
+
+
 int _printf(const char *format, ...);
 #endif
