a45057b6523e5857d3b861755a670ef7bd20c917
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-26 03:46:18 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,5 +1,30 @@
 #include "main.h"
 
+/* get_flag - get format flag
+ * @format: format string
+ * @flags: flags struct
+ *
+ * Return: int
+ */
+int get_flag(const char* s, flags_ty *flags)
+{
+	int i = 0;
+
+	while (s[i] == '+' || s[i] == ' ' || s[i] == '#')
+	{
+		if (s[i] == '+')
+			flags->plus += 1;
+		else if (s[i] == ' ')
+			flags->space += 1;
+		else if(s[i] == '#')
+			flags->hash += 1;
+		i++;
+	}
+	return (i);
+}
+
+
+
 /**
  * _printf - printf replica
  * @format: format
@@ -11,6 +36,7 @@ int _printf(const char *format, ...)
 	int i, len = 0, buf_idx, s_len;
 	va_list args;
 	char buf[1024], c_arg, *s_arg, int_buf[11], binary_buf[32];
+	flags_ty flags;
 
 	va_start(args, format);
 	/*not used variabled*/
@@ -21,6 +47,7 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
+			i += get_flags(&format[i + 1], &flags);
 			if (format[i + 1] == 'c')
 			{
 				c_arg = va_arg(args, int);
