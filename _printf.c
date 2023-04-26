69a414c91d4f745516f26a2862016102521b2421
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-26 02:55:38 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,111 +1,5 @@
 #include "main.h"
 
-/* get_conv_spec - get conversion specefication
- * @format: format string
- * @args; va_list
- *
- * Return: length
- */
-int get_conv_spec(const char *format, va_list args)
-{
-	int i = 0, len = 0, s_len;
-	char c_arg, *s_arg, int_buf[11], binary_buf[32];
-
-	if (format[i + 1] == 'c')
-	{
-		c_arg = va_arg(args, int);
-		if (c_arg)
-		{
-			_putchar(c_arg);
-			len++;
-		}
-	}
-	else if (format[i + 1] == 's')
-	{
-		s_arg = va_arg(args, char *);
-		if (s_arg)
-		{
-			s_len = print_string(s_arg);
-			len += s_len;
-		}
-	}
-	else if (format[i + 1] == 'S')
-	{
-		s_arg = va_arg(args, char *);
-		if (s_arg)
-		{
-			s_len = print_string_non_print(s_arg);
-			len += s_len;
-		}
-	}
-	else if (format[i + 1] == '%')
-	{
-		_putchar('%');
-		len++;
-	}
-	else if (format[i + 1] == 'i' || format[i + 1] == 'd')
-	{
-		int_buf[0] = '\0';
-		len += int_to_string(va_arg(args, int), int_buf);
-		s_len = print_string(int_buf);
-
-	}
-	else if (format[i + 1] == 'b')
-	{
-		binary_buf[0] = '\0';
-		len += int_to_binary(va_arg(args, unsigned int), binary_buf);
-		s_len = print_string(binary_buf);
-	}
-	else if (format[i + 1] == 'o')
-	{
-		binary_buf[0] = '\0';
-		len += int_to_octal(va_arg(args, unsigned int), binary_buf);
-		s_len = print_string(binary_buf);
-	}
-	else if (format[i + 1] == 'u')
-	{
-		binary_buf[0] = '\0';
-		len += uint_to_string(va_arg(args, unsigned int), binary_buf);
-		s_len = print_string(binary_buf);
-	}
-	else if (format[i + 1] == 'x')
-	{
-		binary_buf[0] = '\0';
-		len += int_to_hex(va_arg(args, unsigned int), binary_buf, 0);
-		s_len = print_string(binary_buf);
-	}
-	else if (format[i + 1] == 'X')
-	{
-		binary_buf[0] = '\0';
-		len += int_to_hex(va_arg(args, unsigned int), binary_buf, 1);
-		s_len = print_string(binary_buf);
-	}
-	else if (format[i + 1] == 'p')
-	{
-		len += print_address(va_arg(args, void *));
-	}
-	else if (format[i + 1] == 'r')
-	{
-		s_arg = va_arg(args, char *);
-		if (s_arg)
-		{
-			s_len = print_reverse_string(s_arg);
-			len += s_len;
-		}
-	}
-	else if (format[i + 1] == 'R')
-	{
-		s_arg = va_arg(args, char *);
-		if (s_arg)
-		{
-			s_len = print_rot13ed_string(s_arg);
-			len += s_len;
-		}
-	}
-	return (len);
-}
-
-
 /**
  * _printf - printf replica
  * @format: format
@@ -114,9 +8,9 @@ int get_conv_spec(const char *format, va_list args)
  */
 int _printf(const char *format, ...)
 {
-	int i, len = 0, buf_idx, conve_spec_len;
-	char buf[1024];
+	int i, len = 0, buf_idx, s_len;
 	va_list args;
+	char buf[1024], c_arg, *s_arg, int_buf[11], binary_buf[32];
 
 	va_start(args, format);
 	/*not used variabled*/
@@ -127,12 +21,128 @@ int _printf(const char *format, ...)
 	{
 		if (format[i] == '%')
 		{
-			conve_spec_len = get_conv_spec(&format[i], args);
-			if (conve_spec_len > 0)
+			if (format[i + 1] == 'c')
 			{
-				len += conve_spec_len;
+				c_arg = va_arg(args, int);
+				if (c_arg)
+				{
+					_putchar(c_arg);
+					i++;
+					len++;
+					continue;
+				}
+			}
+			else if (format[i + 1] == 's')
+			{
+				s_arg = va_arg(args, char *);
+				if (s_arg)
+				{
+					s_len = print_string(s_arg);
+					len += s_len;
+					i++;
+					continue;
+				}
+			}
+			else if (format[i + 1] == 'S')
+			{
+				s_arg = va_arg(args, char *);
+				if (s_arg)
+				{
+					s_len = print_string_non_print(s_arg);
+					len += s_len;
+					i++;
+					continue;
+				}
+			}
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				len++;
 				i++;
 				continue;
+			}
+			else if (format[i + 1] == 'i' || format[i + 1] == 'd')
+			{
+				int_buf[0] = '\0';
+				len += int_to_string(va_arg(args, int), int_buf);
+				s_len = print_string(int_buf);
+				/*len += s_len;*/
+				i++;
+				continue;
+			}
+			else if (format[i + 1] == 'b')
+			{
+				binary_buf[0] = '\0';
+				len += int_to_binary(va_arg(args, unsigned int), binary_buf);
+				s_len = print_string(binary_buf);
+				/*len += s_len;*/
+				i++;
+				continue;
+			}
+			else if (format[i + 1] == 'o')
+			{
+				binary_buf[0] = '\0';
+				len += int_to_octal(va_arg(args, unsigned int), binary_buf);
+				s_len = print_string(binary_buf);
+				/*len += s_len;*/
+				i++;
+				continue;
+			}
+			else if (format[i + 1] == 'u')
+			{
+				binary_buf[0] = '\0';
+				len += uint_to_string(va_arg(args, unsigned int), binary_buf);
+				s_len = print_string(binary_buf);
+				/*len += s_len;*/
+				i++;
+				continue;
+			}
+			else if (format[i + 1] == 'x')
+			{
+				binary_buf[0] = '\0';
+				len += int_to_hex(va_arg(args, unsigned int), binary_buf, 0);
+				s_len = print_string(binary_buf);
+				/*len += s_len;*/
+				i++;
+				continue;
+			}
+			else if (format[i + 1] == 'X')
+			{
+				binary_buf[0] = '\0';
+				len += int_to_hex(va_arg(args, unsigned int), binary_buf, 1);
+				s_len = print_string(binary_buf);
+				/*len += s_len;*/
+				i++;
+				continue;
+			}
+			else if (format[i + 1] == 'p')
+			{
+				len += print_address(va_arg(args, void *));
+				/*len += s_len;*/
+				i++;
+				continue;
+			}
+			else if (format[i + 1] == 'r')
+			{
+				s_arg = va_arg(args, char *);
+				if (s_arg)
+				{
+					s_len = print_reverse_string(s_arg);
+					len += s_len;
+					i++;
+					continue;
+				}
+			}
+			else if (format[i + 1] == 'R')
+			{
+				s_arg = va_arg(args, char *);
+				if (s_arg)
+				{
+					s_len = print_rot13ed_string(s_arg);
+					len += s_len;
+					i++;
+					continue;
+				}
 			}
 		}
 			_putchar(format[i]);
