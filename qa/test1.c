cec2f3f971edf1981ece368f9711a1f6a4df0040
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 21:01:56 +0200

    Update test1.c

--- qa/test1.c
+++ qa/test1.c
@@ -4,44 +4,51 @@
 
 int main(void)
 {
-	int i = 2;
+	int i = 3;
 	long int l;
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%d", 1024);
+	printf("\n");
 	printf("%d", 1024);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%d", -1024);
+	printf("\n");
 	printf("%d", -1024);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%d", 0);
+	printf("\n");
 	printf("%d", 0);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%d", INT_MAX);
+	printf("\n");
 	printf("%d", INT_MAX);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%d", INT_MIN);
+	printf("\n");
 	printf("%d", INT_MIN);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	l = (long int)INT_MAX + 1024;
 	_printf("%d", l);
+	printf("\n");
 	printf("%d", l);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	l = (long int)INT_MIN - 1024;
 	_printf("%d", l);
+	printf("\n");
 	printf("%d", l);
 	
 	i++;
@@ -62,26 +69,31 @@ int main(void)
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%i", 1024);
+	printf("\n");
 	printf("%i", 1024);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%i", -1024);
+	printf("\n");
 	printf("%i", -1024);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%i", 0);
+	printf("\n");
 	printf("%i", 0);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%i", INT_MAX);
+	printf("\n");
 	printf("%i", INT_MAX);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%i", INT_MIN);
+	printf("\n");
 	printf("%i", INT_MIN);
 	
 	i++;
@@ -94,6 +106,7 @@ int main(void)
 	printf("\n============== %i ================\n", i);
 	l = (long int)INT_MAX -+ 1024;
 	_printf("%i", l);
+	printf("\n");
 	printf("%i", l);
 	
 	i++;
@@ -124,11 +137,13 @@ int main(void)
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%d", 10000);
+	printf("\n");
 	printf("%d", 10000);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	_printf("%i", 10000);
+	printf("\n");
 	printf("%i", 10000);
 	
 	
