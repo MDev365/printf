75db2373e9fd7b5ef49eb1bf3f74f06b67a07801
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 18:41:55 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -11,6 +11,7 @@ int handle_conversion_specification(const char *format, va_list args, int *ind)
 	i += get_precision(&format[i + 1], &precision, args);
 	i += get_length_modifier(&format[i + 1], &length);
 	i += get_conversion_specifier(&format[i + 1], &flags);
+	i++;
 	if (format[i] == 'c')
 	{
 		c_arg = va_arg(args, int);
