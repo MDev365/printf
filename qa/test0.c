c76a694934a0a6b44bb13cb3be3c1a59a83ad439
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-28 20:05:36 +0200

    Update test0.c

--- qa/test0.c
+++ qa/test0.c
@@ -3,8 +3,79 @@
 
 int main(void)
 {
+	char *str;
 	int i = 3;
 	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("Let's print a simple sentence.\n");
+	printf("Let's print a simple sentence.\n");
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("%c", 'S');
+	printf("%c", 'S');
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("A char inside a sentence: %c. Did it work?\n", 'F');
+	printf("A char inside a sentence: %c. Did it work?\n", 'F');
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	printf("Let'see if the cast is correctly done: %c. Did it work?\n", 48);
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("%s", "This sentence is retrieved from va_args!\n");
+	printf("%s", "This sentence is retrieved from va_args!\n");
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+	printf("Complete the sentence: You %s nothing, Jon Snow.\n", "know");
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	printf("Complete the sentence: You %s nothing, Jon Snow.\n", (char *)0);
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	printf("%c%cth %s%s a%cg%s: Y%sou %s no%ching%s Snow.%c", 'W', 'i', "some ", "more", 'r', "s", "", "know", 't', ", Jon", '\n');
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("%%");
+	printf("%%");
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("Should print a single percent sign: %%\n");
+	printf("Should print a single percent sign: %%\n");
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	printf("%s%c%c%c%s%%%s%c", "Loading ", '.', '.', '.', " 99", " Please wait", '\n');
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("css%ccs%scscscs", 'T', "Test");
+	printf("css%ccs%scscscs", 'T', "Test");
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	str = "Big string";
+	_printf(str);
+	_printf(str);
+	
+	i++;
+	printf("\n============== %i ================\n", i);
+	_printf("man gcc:\n%s", str);
+	printf("man gcc:\n%s", str);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
@@ -18,8 +89,8 @@ int main(void)
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	_printf("t%");
-	printf("m%");
+	_printf("%");
+	printf("%");
 	
 	i++;
 	printf("\n============== %i ================\n", i);
