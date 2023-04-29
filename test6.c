6c07dc139b9f9cb7425d8736a2af47e36a669676
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-29 11:29:19 +0200

    Create test6.c

---
+++ test6.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 on success, error code otherwise
+ */
+int main(void)
+{
+
+	_printf("%ld", 0L);
+  printf("\n");
+	printf("%ld", 0L);
+  printf("\n");
+  
+  _printf("%ld", LONG_MAX);
+  printf("\n");
+  printf("%ld", LONG_MAX);
+   printf("\n");
+ 
+
+	return (0);
+}
