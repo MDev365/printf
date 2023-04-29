6d9daf2ce5824f3a13cf0ed6c1df46276e682f78
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-29 03:28:20 +0200

    Update test2.c

--- qa/test2.c
+++ qa/test2.c
@@ -5,146 +5,55 @@
 int main(void)
 {
 	int i = 3;
-	long int l;
+	long int l, res;
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	_printf("%d", 1024);
+	_printf("%u", 1024);
 	printf("\n");
-	printf("%d", 1024);
+	printf("%u", 1024);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	_printf("%d", -1024);
+	_printf("%u", -1024);
 	printf("\n");
-	printf("%d", -1024);
+	printf("%u", -1024);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	_printf("%d", 0);
+	_printf("%u", 0);
 	printf("\n");
-	printf("%d", 0);
+	printf("%u", 0);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	_printf("%d", INT_MAX);
+	_printf("%u", UINT_MAX);
 	printf("\n");
-	printf("%d", INT_MAX);
+	printf("%u", UINT_MAX);
 	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%d", INT_MIN);
-	printf("\n");
-	printf("%d", INT_MIN);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
 	l = (long int)INT_MAX + 1024;
-	_printf("%d", l);
+	_printf("%u", l);
 	printf("\n");
-	printf("%d", l);
+	printf("%u", l);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	l = (long int)INT_MIN - 1024;
-	_printf("%d", l);
-	printf("\n");
-	printf("%d", l);
+	_printf("There is %u bytes in %u KB\n", 1024, 1);
+	printf("There is %u bytes in %u KB\n", 1024, 1);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	_printf("There is %d bytes in %d KB\n", 1024, 1);
-	printf("There is %d bytes in %d KB\n", 1024, 1);
+	_printf("%u - %u = %u\n", 2048, 1024, 1024);
+	printf("%u - %u = %u\n", 2048, 1024, 1024);
 	
 	i++;
 	printf("\n============== %i ================\n", i);
-	_printf("%d - %d = %d\n", 1024, 2048, -1024);
-	printf("%d - %d = %d\n", 1024, 2048, -1024);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	printf("%d + %d = %d\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i", 1024);
-	printf("\n");
-	printf("%i", 1024);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i", -1024);
-	printf("\n");
-	printf("%i", -1024);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i", 0);
-	printf("\n");
-	printf("%i", 0);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i", INT_MAX);
-	printf("\n");
-	printf("%i", INT_MAX);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i", INT_MIN);
-	printf("\n");
-	printf("%i", INT_MIN);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	l = (long int)INT_MAX + 1024;
-	_printf("%i", l);
-	printf("%i", l);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	l = (long int)INT_MAX -+ 1024;
-	_printf("%i", l);
-	printf("\n");
-	printf("%i", l);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("There is %i bytes in %i KB\n", 1024, 1);
-	printf("There is %i bytes in %i KB\n", 1024, 1);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i - %i = %i\n", 1024, 2048, -1024);
-	printf("%i - %i = %i\n", 1024, 2048, -1024);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	printf("%i + %i = %i\n", INT_MIN, INT_MAX, (INT_MIN + INT_MAX));
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("iddi%diddiiddi\n", 1024);
-	printf("iddi%diddiiddi\n", 1024);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%d", 10000);
-	printf("\n");
-	printf("%d", 10000);
-	
-	i++;
-	printf("\n============== %i ================\n", i);
-	_printf("%i", 10000);
-	printf("\n");
-	printf("%i", 10000);
+	res = (long int) INT_MAX * 2;
+	_printf("%u + %u = %u\n", INT_MAX, INT_MAX, res);
+	printf("%u + %u = %u\n", INT_MAX, INT_MAX, res);
 	
 	
 	printf("\n");
