34b55c1cab9dd5cffd8de5fa5e77bcb0472d2bfd
MDev365 <72352419+MDev365@users.noreply.github.com>
2023-04-29 03:49:42 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -10,7 +10,7 @@
 */
 int handle_conversion_specification(const char *format, va_list args, int *ind, int *conv_len)
 {
-	int i = *ind, width = 0, precision = -1, len = 0, conv_exist = 1;
+	int i = *ind, width = 0, precision = -1, len = 0, s_len, conv_exist = 1;
 	flags_ty flags = {0, 0, 0, 0, 0, 0};
 	char c_arg, int_buf[20], binary_buf[32], length;
 
